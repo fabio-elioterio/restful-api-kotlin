a555a0ec24918882b4e38a0f7a8a93a88008606e
fabio <fabioelioterio.77@gmail.com>
2020-06-05 12:20:55 -0300

    Construção da camada service e testes para lançamento

---
+++ pontointeligente/src/main/kotlin/br/com/zup/pontointeligente/services/impl/LancamentoServiceImpl.kt
@@ -0,0 +1,20 @@
+package br.com.zup.pontointeligente.services.impl
+
+import br.com.zup.pontointeligente.documents.Lancamento
+import br.com.zup.pontointeligente.repositories.LancamentoRepository
+import br.com.zup.pontointeligente.services.LancamentoService
+import org.springframework.data.domain.Page
+import org.springframework.data.domain.PageRequest
+import org.springframework.stereotype.Service
+
+@Service
+class LancamentoServiceImpl(val lancamentoRepository: LancamentoRepository) : LancamentoService {
+    override fun buscarPorFuncionarioId(funcionarioId: String, pageRequest: PageRequest): Page<Lancamento> =
+            lancamentoRepository.findByFuncionarioId(funcionarioId, pageRequest)
+
+    override fun buscarPorId(id: String): Lancamento? = lancamentoRepository.findById(id).get()
+
+    override fun persistir(lancamento: Lancamento): Lancamento = lancamentoRepository.save(lancamento)
+
+    override fun remover(id: String) = lancamentoRepository.deleteById(id)
+}
