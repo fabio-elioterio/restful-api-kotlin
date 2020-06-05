9897331092624369b74937dd450f989607ce589f
fabio <fabioelioterio.77@gmail.com>
2020-06-05 15:10:34 -0300

    Criação de DTOs

--- pontointeligente/build.gradle.kts
+++ pontointeligente/build.gradle.kts
@@ -3,6 +3,7 @@ import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 plugins {
 	id("org.springframework.boot") version "2.3.0.RELEASE"
 	id("io.spring.dependency-management") version "1.0.9.RELEASE"
+	id("org.hibernate.gradle.tools") version "1.2.5"
 	kotlin("jvm") version "1.3.72"
 	kotlin("plugin.spring") version "1.3.72"
 }
@@ -22,6 +23,7 @@ dependencies {
 	implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
 	implementation("org.jetbrains.kotlin:kotlin-reflect")
 	implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
+	implementation("org.hibernate.validator:hibernate-validator:6.1.2.Final")
 	testImplementation("org.springframework.boot:spring-boot-starter-test") {
 		exclude(group = "org.junit.vintage", module = "junit-vintage-engine")
 	}
---
+++ pontointeligente/src/main/kotlin/br/com/zup/pontointeligente/dtos/EmpresaDto.kt
@@ -0,0 +1,7 @@
+package br.com.zup.pontointeligente.dtos
+
+data class EmpresaDto (
+        val razaoSocial: String,
+        val cnpj: String,
+        val id: String? = null
+)
